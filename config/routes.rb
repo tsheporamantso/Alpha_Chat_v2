583fd6ebedc8394b6332677e0125faacd0f077af
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-08 15:26:17 +0200

    Expose articles index resourcesful route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
+  resources :articles, only: [:index]
+
   get "about", to: "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
