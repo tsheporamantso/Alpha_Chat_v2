2234c8a4e19c77008b31fe7ebae33b2e61bb28c1
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-08 17:19:47 +0200

    Add before action callback method to set article and keep code DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -23,12 +24,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       flash[:notice] = "Article updated sucessfully 🎉."
       redirect_to article_path(@article)
@@ -38,8 +36,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
-
     if @article.destroy
       flash[:notice] = "Article was deleted successfully 🎉."
       redirect_to articles_path
@@ -53,5 +49,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
