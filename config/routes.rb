39af1977d1b35200bf3492eefb666e01d8ce1f28
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-08 16:03:16 +0200

    Expose edit and update article resourcesful route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
-  resources :articles, only: [:index, :show, :new, :create]
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
 
   get "about", to: "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
