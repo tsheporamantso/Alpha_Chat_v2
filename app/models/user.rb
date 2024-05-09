6cf6340cc58c2b02f14b6b345f41a922085c3f95
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-09 20:51:32 +0200

    Change state of email object to always be on downcase before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { in: 3...25 }
