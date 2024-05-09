35687cb9257db1804b886c8d560c0f6ca74881a6
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-09 20:21:44 +0200

    Hard coded first user for article creation until I set user authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
 
     if @article.save
       flash[:notice] = 'Article was created successfully 🎉.'
