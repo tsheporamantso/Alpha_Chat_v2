9aa9ca816189ff3901ead506fe958a6809d97746
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-09 18:30:20 +0200

    Generate User model also add associations between users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { in: 6...100 }
   validates :description, presence: true, length: { in: 10...300 }
+
+  belongs_to :user
 end
