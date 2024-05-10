4889bb7fe9e9e91245a9bd0b657e81346a62db04
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-10 13:45:55 +0200

    Add has secure password method on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
-
+  has_secure_password
   has_many :articles
 end
