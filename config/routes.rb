0bceb7a78f520db12a12b88cd120e75e1d9bd7d2
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-10 15:09:18 +0200

    Add get signup users route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   get "about", to: "pages#about"
+
+  get "signup", to: "users#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
