5a9c0cefe28fee68be44d323c70b9f2eb896c88b
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-13 14:14:00 +0200

    Correct typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:sessions] [:email].downcase)
-    if user && user.authenticate(params[:sessions] [:password])
+    user = User.find_by(email: params[:session] [:email].downcase)
+    if user && user.authenticate(params[:session] [:password])
       session[:user_id] = user.id
       flash[:notice] = "Logged in successfully"
       redirect_to user
