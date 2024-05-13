68a2d2cd0dab83dd403bae4ec339388224f9fbac
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-13 12:21:28 +0200

    Define create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:sessions] [:email].downcase)
+    if user && user.authenticate(params[:sessions] [:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 end
