582e7889dd3f572f43362f554fb129527460a043
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-13 21:25:16 +0200

    Define require same user action and add before action call back

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
   before_action :require_user, except: %i[index show]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 3)
@@ -53,5 +54,13 @@ class ArticlesController < ApplicationController
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @article.user
+
+    flash[:alert] = 'You can only edit or delete your own articles.'
+    redirect_to article_path(@article)
+   end
   end
 end
