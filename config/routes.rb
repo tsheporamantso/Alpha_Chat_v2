5b8ccc8325ed2015c3d04bcc2a3009ca149d403a
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-13 11:15:18 +0200

    Add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,18 @@ Rails.application.routes.draw do
 
   resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
-  get "about", to: "pages#about"
+  resources :users, except: [:new]
 
   get "signup", to: "users#new"
-  resources :users, except: [:new]
+
+  get "about", to: "pages#about"
+
+  get 'login', to: "sessions#new"
+
+  post 'login', to: "sessions#create"
+
+  delete 'logout', to: "sessions#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
