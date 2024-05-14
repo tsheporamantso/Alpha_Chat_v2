94e18fd40e860caaafb7ee96434e04788c4e5c5d
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-14 19:53:25 +0200

    Add category validation test

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-
+validates :name, presence: true, length: { maximum: 30 }, uniqueness: { case_sensitive: false}
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -7,4 +7,20 @@ class CategoryTest < ActiveSupport::TestCase
     assert @category.valid?
   end
 
+  test 'it should not be valid without the name' do
+    @category = Category.new(name: '')
+    assert_not @category.valid?
+  end
+
+  test 'name should be unique' do
+    @category = Category.new(name: 'Sport')
+    @category.save
+    @category2 = Category.new(name: 'Sport')
+    assert_not @category2.valid?
+  end
+
+  test 'length should not be too long' do
+    @category = Category.new(name: 'a' * 40)
+    assert_not @category.valid?
+  end
 end
