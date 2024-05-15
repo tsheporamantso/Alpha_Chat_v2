3718324de13a033d78b7e60e7cfbd2afc5ea4acf
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-15 18:34:22 +0200

    Add categories listing test case

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @category = Category.create(name: "Sports")
+    @category2 = Category.create(name: "Travel")
+  end
+
+  test 'should show categories listing' do
+    
+    get categories_path
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
 end
