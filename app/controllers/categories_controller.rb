f4f51f22992290855df6ab705031cdf0ef050641
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-16 15:50:23 +0200

    Define categories create action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,12 +4,30 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = 'Category was successfully created'
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
