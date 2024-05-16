7fa02f9a72b97cb19b10d2d1723093bcd9c73e33
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-16 12:54:48 +0200

    Generate article test unit scaffold, all test failed.

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,49 @@
-require 'test_helper'
+require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  # setup do
+  #   @user = User.create(username: 'johndoe', email: 'john.doe@example.com', password: 'password', admin: true)
+  #   @article = Article.create(title: 'Example Title', description: 'Example Description', user_id: @user.id)
+  # end
+
+  # test "should get index" do
+  #   get articles_url
+  #   assert_response :success
+  # end
+
+  # test "should get new" do
+  #   get new_article_url
+  #   assert_response :success
+  # end
+
+  # test "should create article" do
+  #   assert_difference("Article.count", 1) do
+  #     post articles_url, params: { article: { title: 'Example Title2', description: 'Example Description2', user_id: @user.id } }
+  #   end
+
+  #   assert_redirected_to article_url(Article.last)
+  # end
+
+  # test "should show article" do
+  #   get article_url(@article)
+  #   assert_response :success
+  # end
+
+  # test "should get edit" do
+  #   get edit_article_url(@article)
+  #   assert_response :success
+  # end
+
+  # test "should update article" do
+  #   patch article_url(@article), params: { article: {  } }
+  #   assert_redirected_to article_url(@article)
+  # end
+
+  # test "should destroy article" do
+  #   assert_difference("Article.count", -1) do
+  #     delete article_url(@article)
+  #   end
+
+  #   assert_redirected_to articles_url
   # end
 end
