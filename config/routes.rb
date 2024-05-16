b5b049493a741cd29876132e28616bf21b023d04
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-16 14:12:22 +0200

    Remove categories resourceful rest routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   get 'login' => "sessions#new"
   post 'login' => "sessions#create"
   delete 'logout' => "sessions#destroy"
-  resources :categories, except: [:destroy]
 
 end
