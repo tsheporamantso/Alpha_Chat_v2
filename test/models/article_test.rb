6273d3f03fb674da17ce2650b27373ebbafbf528
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-16 14:21:42 +0200

    Correct rubocop offenses

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-   
   setup do
-    @article = Article.new(title: "John Doe", description: "John is the best and famous web developer in the world")
+    @article = Article.new(title: 'John Doe', description: 'John is the best and famous web developer in the world')
   end
 
   test 'article should be valid' do
@@ -11,26 +10,26 @@ class ArticleTest < ActiveSupport::TestCase
   end
 
   test 'it should not be valid without the title' do
-    @article.title = ""
+    @article.title = ''
   end
 
-  test  'it should not be valid without the description' do
-    @article.description = ""
+  test 'it should not be valid without the description' do
+    @article.description = ''
   end
 
-  test  'title should not be too long' do
-    @article.title = "a" * 101
+  test 'title should not be too long' do
+    @article.title = 'a' * 101
   end
 
   test 'title should not be too short' do
-    @article.title = "2" * 5
+    @article.title = '2' * 5
   end
 
   test 'description should not be too long' do
-    @article.description = "w" * 501
+    @article.description = 'w' * 501
   end
 
-  test 'description should not be too short' do 
-    @article.description = "w" * 9
+  test 'description should not be too short' do
+    @article.description = 'w' * 9
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,15 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
-  setup do 
+  setup do
     @user = User.new(username: 'johndoe', email: 'john.doe@example.com', password: 'password')
   end
 
   test 'username should be unique and case sensitive' do
-   @user.save
-   @user2 = User.new(username: 'JohnDoe')
-   assert_not @user2.valid?
+    @user.save
+    @user2 = User.new(username: 'JohnDoe')
+    assert_not @user2.valid?
   end
 
   test 'email should be unique and case insensitive' do
