da7f9c082c188807f0572dc8de5b689d98b20b7e
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-16 20:54:43 +0200

    Add admin user requirement and test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  
   setup do
     @category = Category.create(name: 'Technology')
+    @admin_user = User.create(username: 'johndoe', email: 'johndoe@example.com', password: 'password', admin: true)
   end
 
   test 'it should get index' do
@@ -11,6 +13,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'it should get new' do
+    sign_in_as(@admin_user)
     get new_category_path
     assert_response :success
   end
@@ -26,9 +29,17 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   # end
 
   test 'should create category' do
+    sign_in_as(@admin_user)
     assert_difference('Category.count', 1) do
       post categories_path, params: { category: { name: 'Travel' } }
     end
     assert_redirected_to category_path(Category.last)
+  end
+
+  test 'should not create category if not admin' do
+    assert_no_difference('Category.count') do
+      post categories_path, params: { category: { name: 'Travel' } }
+    end
+    assert_redirected_to categories_path
   end
 end
