ca6025a0aad561de9158f84d5694757e7b5575ca
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-16 11:33:39 +0200

    Test username and email uniqueness and case sensitivity

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do 
+    @user = User.new(username: 'johndoe', email: 'john.doe@example.com', password: 'password')
+  end
+
+  test 'username should be unique and case sensitive' do
+   @user.save
+   @user2 = User.new(username: 'JohnDoe')
+   assert_not @user2.valid?
+  end
+
+  test 'email should be unique and case insensitive' do
+    @user.save
+    @user2 = User.new(email: 'John.Doe@example.com')
+    assert_not @user2.valid?
+  end
 end
