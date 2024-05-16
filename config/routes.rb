fa234f81543190e631e3df8ba2bee95327ca02e6
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-16 14:39:20 +0200

    Add categories resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'login' => "sessions#new"
   post 'login' => "sessions#create"
   delete 'logout' => "sessions#destroy"
+  resources :categories, except: [:destroy]
 
 end
