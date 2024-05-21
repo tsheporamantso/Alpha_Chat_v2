4f4581058379e3153f22f1c0ac69fc63a91ab9fe
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-21 11:39:15 +0200

    Add association between article, category and article_categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ApplicationRecord
   validates :description, presence: true, length: { in: 10...500 }
 
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
